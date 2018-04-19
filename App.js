b468cdcb19a1f579f5bd250acec13619e2839cdc
iheno <henorodel@gmail.com>
2018-04-19 17:52:25 +0900

    make Appbar.js & CircleButton.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Appbar from './src/components/Appbar';
 import MemoList from './src/components/MemoList';
+import CircleButton from './src/elements/CircleButton';
 
 // import BodyText from './src/elements/BodyText';
 
@@ -12,15 +15,9 @@ export default class App extends React.Component {
         <Text> MemoApp! Start! </Text>
         <BodyText>Hi! Heno!</BodyText>
         */}
-        <View style={styles.appbar}>
-          <Text style={styles.appbarTitle}>MEMO</Text>
-        </View>
-
+        <Appbar />
         <MemoList />
-
-        <View style={styles.memoAddButton}>
-          <Text style={styles.memoAddButtonTitle}>+</Text>
-        </View>
+        <CircleButton>+</CircleButton>
 
       </View>
     );
@@ -33,46 +30,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  memoAddButton: {
-    position: 'absolute',
-    bottom: 32,
-    right: 32,
-    width: 48,
-    height: 48,
-    backgroundColor: 'rgba(247, 36, 117, 1)',
-    borderRadius: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  memoAddButtonTitle: {
-    fontSize: 32,
-    color: '#fff',
-    lineHeight: 32,
-  },
-  appbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 70,
-    paddingTop: 30,
-    backgroundColor: '#f8f8f8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-  appbarTitle: {
-    color: '#101010',
-    fontSize: 16,
-    fontWeight: '600',
   },
 });
---
+++ src/components/Appbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class Appbar extends React.Component {
+  render() {
+    return (
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarTitle}>MEMO</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 70,
+    paddingTop: 30,
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#101010',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+
+export default Appbar;
